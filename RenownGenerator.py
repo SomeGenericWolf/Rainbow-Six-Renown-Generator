ee520a9886946f6ac55888d6aeca9294291f8c38
SomeGenericWolf <somegenericwolf@gmail.com>
2020-02-04 17:49:23 -0600

    Multiple path support
    Added the support for multiple paths for the .exe, and changed some of the settings of the game to get a better performance.

--- RenownGenerator.py
+++ RenownGenerator.py
@@ -23,7 +23,34 @@ for element in os.listdir(ext_dir_folder):
 if game_uuid is None:
     exit(f"Error. UUID not found.")
 dir_folder = f"{user_profile}\\Documents\\My Games\\Rainbow Six - Siege\\{game_uuid}"
-r6s_launcher = f"C:\\Program Files (x86)\\Steam\\steamapps\\common\\Tom Clancy's Rainbow Six Siege\\RainbowSix.exe"
+steam_dir = "\\Program Files (x86)\\Steam\\steamapps\\common\\Tom Clancy's Rainbow Six Siege"
+uplay_dir = "\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\games\\Tom Clancy's Rainbow Six Siege"
+
+if os.path.exists(f"C:{steam_dir}"):
+    r6s_launcher = f"C:{steam_dir}\\RainbowSix.exe"
+elif os.path.exists(f"D:{steam_dir}"):
+    r6s_launcher = f"D:{steam_dir}\\RainbowSix.exe"
+elif os.path.exists(f"C:{uplay_dir}"):
+    r6s_launcher = f"C:{uplay_dir}\\RainbowSix.exe"
+elif os.path.exists(f"D:{uplay_dir}"):
+    r6s_launcher = f"D:{uplay_dir}\\RainbowSix.exe"
+else:
+    try:
+        r6s_launcher = open("Files\\R6S_Path.txt").read().strip()
+    except IOError:
+        print(f"Enter the path of the folder where \"Rainbow Six\" is installed.")
+        r6s_launcher = input(f"\nExample: C:{steam_dir}\nEnter the path: ")
+        r6s_launcher = r6s_launcher.replace('\"', '')
+        print(f"\nLooking for \"RainbowSix.exe\" in: \"{r6s_launcher}\"")
+        if os.path.isfile(f"{r6s_launcher}\\RainbowSix.exe"):
+            print("\"RainbowSix.exe\" found.")
+            r6s_launcher = f"{r6s_launcher}\\RainbowSix.exe"
+            with open("Alternative stuff\\R6S_Path.txt", 'w') as f:
+                f.write(r6s_launcher)
+        else:
+            print("\"RainbowSix.exe\" not found inside the path you provided. Exiting")
+            time.sleep(3.3)
+            exit()
 
 
 def setting_config():
@@ -34,7 +61,6 @@ def setting_config():
         print(f"Creating a backup of the Configuration at \"{dir_folder}\\GameSettings.bk\"...")
         shutil.copy(f"{dir_folder}\\GameSettings.ini", f"{dir_folder}\\GameSettings.bk")
         config = configparser.ConfigParser()
-
         config.read(f"{dir_folder}\\GameSettings.ini")
         config.set("DISPLAY_SETTINGS", "WindowMode", "1")
         config.set("DISPLAY_SETTINGS", "ResolutionWidth", "1280")
@@ -44,9 +70,16 @@ def setting_config():
         config.set("DISPLAY_SETTINGS", "InitialWindowPositionY", "-1")
         config.set("QUALITY_SETTINGS", "TexturePresetName", "Low")
         config.set("CUSTOM_PRESET_SETTINGS", "Shadow", "0")
+        config.set("CUSTOM_PRESET_SETTINGS", "Reflection", "0")
+        config.set("CUSTOM_PRESET_SETTINGS", "LOD", "0")
+        config.set("CUSTOM_PRESET_SETTINGS", "AO", "0")
+        config.set("CUSTOM_PRESET_SETTINGS", "Shading", "0")
+        config.set("CUSTOM_PRESET_SETTINGS", "MotionBlur", "0")
+        config.set("CUSTOM_PRESET_SETTINGS", "LensEffects", "0")
+        config.set("CUSTOM_PRESET_SETTINGS", "DepthOfField", "0")
+        config.set("CUSTOM_PRESET_SETTINGS", "DepthOfField", "0")
         config.set("DISPLAY", "FPSLimit", "30")
         config.set("AUDIO", "Volume", "0")
-
         print(f"New parameters written.")
         with open(f'{dir_folder}\\GameSettings.ini', 'w') as configfile:
             config.write(configfile)
@@ -58,14 +91,12 @@ def skipping_intro():
     st_presence = False
     main_menu_presence = False
     search_and_click("warning.PNG", 1000)
-
     while st_presence is False:
         if pyautogui.locateCenterOnScreen(f"{fd}servers_text.PNG", confidence=0.8) is None:
             pyautogui.press("u", presses=5, interval=0.01)
             time.sleep(0.3)
         else:
             st_presence = True
-
     while main_menu_presence is False:
         if pyautogui.locateCenterOnScreen(f"{fd}menu_button.PNG", confidence=0.8) is None:
             pass
@@ -99,13 +130,11 @@ def renown_generator():
     print("\nRenown Generator Started. - (To stop, press \"Esc\" while in-game. or CTRL+C)")
     matches = 0
     stop = False
-
     while stop is False:
         match_end = False
         search_and_click("res.PNG", 300)
         search_and_click("rook.PNG")
         search_and_click("confirm.PNG")
-
         while match_end is False:
             failure_text = pyautogui.locateCenterOnScreen(f"{fd}failure.PNG", confidence=0.8)
             quit_to_desk = pyautogui.locateCenterOnScreen(f"{fd}quit.PNG", confidence=0.8)
@@ -113,6 +142,8 @@ def renown_generator():
                 pass
             else:
                 print(f"Closing Program, {matches} Matches Played.")
+                search_and_click("quit.PNG")
+                search_and_click("confirm_quit.PNG")
                 stop = True
                 match_end = True
             if failure_text is None:
@@ -123,11 +154,6 @@ def renown_generator():
                 search_and_click("close.PNG")
                 search_and_click("bonus.PNG")
                 search_and_click("votefor.PNG")
-
-
-def closing_game():
-    search_and_click("quit.PNG")
-    search_and_click("confirm_quit.PNG")
 
 
 def restoring_config():
@@ -153,7 +179,6 @@ try:
     skipping_intro()
     starting_game()
     renown_generator()
-    closing_game()
     restoring_config()
 except KeyboardInterrupt:
     print("You pressed CTRL+C.")
